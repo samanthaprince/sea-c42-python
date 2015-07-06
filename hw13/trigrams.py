64560d10772d3823e953e5f3c79148b8093b87f1
Samantha Prince <Samanthaprince928@gmail.com>
2015-07-05 17:56:58 -0700

    Added generate new text function

--- hw13/trigrams.py
+++ hw13/trigrams.py
@@ -8,15 +8,26 @@ def read_text(lines):
     for x in range(0, len(lines) - 1):
         words = lines[x].split()
         for y in range(0, len(words) - 2):
-            d[(words[y], words[y + 1])] = [words[i + 2]]
-    # output(d)
+            d[(words[y], words[y + 1])] = [words[y + 2]]
+    generate_new_text(d)
 
 
 def generate_new_text(d):
-
+    new_text_story = ""
+    for x in range(5):
+        new_sentence = list(random.choice(list(d.keys())))
+        new_sentence[0] = new_sentence[0].capitalize()
+        for y in range(random.randint(5, 10)):
+            words = random.choice(list(d.keys()))
+            new_sentence.extend(words)
+        new_sentence[-1] += ".  "
+        new_sentence = " ".join(new_sentence)
+        new_text_story += new_sentence
+    print(new_text_story)
 
 if __name__ == '__main__':
     try:
+        # can also use file sherlock_small.txt
         f = open('sherlock.txt', 'r')
         lines = f.readlines()
         f.close()
