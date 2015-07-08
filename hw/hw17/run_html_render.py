5e890dfb59e6906a9594f3cb282701d42afd6882
henrykh <henryhowes@gmail.com>
2015-07-08 10:32:46 -0700

    removes unicode conversion

--- hw/hw17/run_html_render.py
+++ hw/hw17/run_html_render.py
@@ -38,11 +38,11 @@ def render(page, filename):
 
 page = hr.Element()
 
-page.append(u"Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text")
+page.append("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text")
 
-page.append(u"And here is another piece of text -- you should be able to add any number")
+page.append("And here is another piece of text -- you should be able to add any number")
 
-render(page, u"test_html_output1.html")
+render(page, "test_html_output1.html")
 
 # ## Step 2
 # ##########
