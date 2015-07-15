d3b1b2db5e29156fbaff9215f3021d9e78017ae6
Samantha Prince <Samanthaprince928@gmail.com>
2015-07-14 21:43:48 -0700

    Everything works, but wont print letter in terminal

--- hw012/update_mailroom.py
+++ hw012/update_mailroom.py
@@ -1,17 +1,28 @@
+donor_list = {
+    'Margie DeBella': [100, 95],
+    'Liz Rogers': [1000, 5000, 25],
+    'Lisa Stokes': [800],
+    'Fred Flinstone': [2000, 400, 1500]
+}
+
+
 def start_screen():
     """Opening screen to inform user of program options."""
     opener = "Welcome to Mailroom Madness"
     menu_option = """
     Choose from the following:\n
-    T - Send a (T)hank You\n
-    R - Create a (R)eport\n
-    quit - Quit the program\n"""
+    T - Send a (T)hank You
+    R - Create a (R)eport
+    quit - Quit the program\n
+    Input:  """
 
     print(opener)
+
     path = input(menu_option)
+    check_valid_path(path)
 
     while path:
-        path = input("Please enter from the selections given\n" + menu_option)
+        path = input("Please enter from the selections below:\n" + menu_option)
         path = check_valid_path(path)
 
 
@@ -20,7 +31,7 @@ def check_valid_path(path):
     try:
         path = path.lower()
         if path == "q" or path == "quit":
-            exit()
+            quit()
         elif path == "r":
             generate_report()
         elif path == "t":
@@ -30,22 +41,33 @@ def check_valid_path(path):
     except TypeError:
         return unvalid
 
+
 def thankyou():
     menu_option = """
     Please enter a name or choose from the following:\n
-    list - Print a list of previous donors\n
-    quit - Return to the main menu\n"""
+    list - Print a list of previous donors
+    quit - Return to the main menu\n
+    Input: """
 
     print(menu_option)
 
-    path = input()
+    donor = input()
+    check_thankyou_path(donor)
 
-donor_list = {
-    'Margie DeBella': [100, 95],
-    'Liz Rogers': [1000, 5000, 25],
-    'Lisa Stokes': [800],
-    'Fred Flinstone': [2000, 400, 1500]
-}
+
+def check_thankyou_path(donor):
+
+    if donor.lower() == "quit":
+        start_screen()
+    elif donor.lower() == "list":
+        print("See below for a list of donor names")
+        print(donor_name_only())
+        thankyou()
+    try:
+        if donor_list[donor]:
+            donor_add(donor)
+    except KeyError:
+        donor_add(donor)
 
 
 def generate_report():
@@ -63,6 +85,7 @@ def generate_report():
         print('%s \t|$%d \t|%d | $%d' %
               (donor_name, total_donated, num_donate, avg))
     return False
+    start_screen()
 
 
 def donor_name_only():
@@ -71,62 +94,54 @@ def donor_name_only():
         print(key)
 
 
-def donor_add(donor, amount):
+def donor_add(donor):
     """Add donor to database"""
-    donor_list[donor] = [amount]
+    if donor not in donor_list:
+        donor_list[donor] = []
+        donation = donation_add(donor)
+        while not isinstance(donation, float) or donation < 0:
+            donation = donation_add(donor)
+
+    donor_list[donor].append[donation]
+    print_letter(donor, donation)
 
 
-def donation_add(donor, amount):
+def donation_add(donor):
     """Adds amount of donation to donor donation list."""
-    donor_list[donor].append(amount)
+    donation = input("What is the donation amount? ")
+    try:
+        return float(donation)
+    except ValueError:
+        print("Invalid! Please enter a number. ")
+        return None
 
 
-def print_letter(donor, amount):
+def print_letter(donor, donation):
     """Prints email thanking donor for donation."""
-    print("Dear %s,\n\n"
-          "Thank you so much for your kind donation of $%s \n"
-          "to the Chester County Fund for Women and Girls.\n"
-          "Without your help, women and girls in Chester County would not\n"
-          "thrive or live up to their fullest potential.\n\n"
-          "We hope you will continue to make this foundation a top\n"
-          "priority of yours for years to come.\n\n"
-          "Best regards,\n"
-          "Samantha Prince\n"
-          "Grants Manager" % (donor, amount))
+    str(donation)
+
+    letter = ("""Dear %s,
+
+    Thank you so much for your kind donation of $%s
+    to the Chester County Fund for Women and Girls.
+
+    Without your help, women and girls in Chester County would not
+    thrive or live up to their fullest potential.
+
+    We hope you will continue to make this foundation a top
+    priority of yours for years to come.
+
+    Best regards,
+    Samantha Prince
+    Grants Manager""" % (donor, donation))
+
+    with open(donor + ".txt", "w") as f:
+        f.write(letter)
+
+    print(letter)
+    # start_screen()
 
 
 if __name__ == '__main__':
 
     start_screen()
-
-
-    elif (path.upper() == 'T'):
-
-
-        if (donor.lower() == 'q' or donor.lower() == 'quit'):
-            start_screen()
-
-        elif (donor.lower() == 'l' or donor.lower() == 'list'):
-            print("See below for a list of donors: ")
-            print(donor_name_only())
-
-        else:
-            while True:
-                try:
-                    amount = int(input("Please enter a donation amount: "))
-                except ValueError:
-                    print("Invalid! Please enter a number.")
-                    continue
-                else:
-                    break
-
-            if donor in donor_list:
-                donation_add(donor, amount)
-                print_letter(donor, amount)
-            else:
-                donor_add(donor, amount)
-                donation_add(donor, amount)
-                print_letter(donor, amount)
-
-        if (path.lower == 'q' or path.lower() == 'quit'):
-            quit()
